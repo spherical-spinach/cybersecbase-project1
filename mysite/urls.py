3b374f9bc5ead7e371ceafebbb4ab75ed4da943f
iemeli <eemeli.paalimaki.work@protonmail.com>
2024-05-28 20:32:19 +0300

    Change all views to function-based views

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,10 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.contrib.auth.views import LoginView
 
 urlpatterns = [
-    path('polls/', include('polls.urls')),
+    path('', include('polls.urls')),
     path('admin/', admin.site.urls),
     path('__debug__/', include('debug_toolbar.urls')),
-    path('', include('polls.urls')),
 ]
