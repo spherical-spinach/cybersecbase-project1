7a8ea70efb3e8e100becb27e4f9a94618a0c4543
iemeli <eemeli.paalimaki.work@protonmail.com>
2024-05-28 15:37:00 +0300

    Polish styling, add User and Vote model, modify urls

--- mysite/urls.py
+++ mysite/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
     path('__debug__/', include('debug_toolbar.urls')),
+    path('', include('polls.urls')),
 ]
--- polls/models.py
+++ polls/models.py
@@ -3,6 +3,7 @@ import datetime
 from django.db import models
 from django.utils import timezone
 from django.contrib import admin
+from django.contrib.auth.models import AbstractUser
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
@@ -28,3 +29,16 @@ class Choice(models.Model):
 
     def __str__(self):
         return self.choice_text
+    
+class User(AbstractUser):
+    pass
+
+class Vote(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice = models.ForeignKey(Choice, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        unique_together = ('user', 'question')
