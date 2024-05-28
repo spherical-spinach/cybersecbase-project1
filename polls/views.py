5033f9b7169284e832526ee5484e4da9d73f6102
iemeli <eemeli.paalimaki.work@protonmail.com>
2024-05-28 21:28:47 +0300

    Add broken access control to detail and results views

--- polls/views.py
+++ polls/views.py
@@ -14,12 +14,12 @@ def index(request):
     context = {'latest_question_list': latest_question_list}
     return render(request, 'polls/index.html', context)
 
-
+# @login_required
 def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/detail.html', {'question': question})
 
-
+# @login_required
 def results(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/results.html', {'question': question})
