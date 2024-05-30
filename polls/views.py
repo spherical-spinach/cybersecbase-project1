d5586caec4dbac654d4965d07f7292c1b0690402
iemeli <eemeli.paalimaki.work@protonmail.com>
2024-05-30 11:33:08 +0300

    Remove unneseccary comments

--- polls/views.py
+++ polls/views.py
@@ -47,7 +47,6 @@ def vote(request, question_id):
     try:
         selected_choice = question.choice_set.get(pk=request.POST["choice"])
     except (KeyError, Choice.DoesNotExist):
-        # Redisplay the question voting form.
         return render(
             request,
             "polls/detail.html",
@@ -59,9 +58,6 @@ def vote(request, question_id):
     else:
         selected_choice.votes = F("votes") + 1
         selected_choice.save()
-        # Always return an HttpResponseRedirect after successfully dealing
-        # with POST data. This prevents data from being posted twice if a
-        # user hits the Back button.
         return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
     
 @login_required
